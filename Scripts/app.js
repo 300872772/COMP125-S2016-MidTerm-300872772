4e15251c461c41ab2818b60ced4488020faddcec
300872772 <mamun_131@hotmail.com>
2016-06-22 13:35:39 -0400

    added itroduction paragraph

--- Scripts/app.js
+++ Scripts/app.js
@@ -19,7 +19,29 @@
     function InitialFunction() {
 
         ContatctFormEventHandling();
+        InsertParagraphData();
     }
+
+
+function InsertParagraphData(){
+
+    var IntroParagraph = document.getElementById("IntroParagraph");
+
+var introParaData = "Now is the time to travel to Greece." 
++"The country may be going through an economic crisis,"
++"but many travellers say that it hasn't impacted the experience of visiting."+
+"Plus, there may be some great deals." 
++"Greece has 1,400 islands, though only 230 of them are inhabited." 
++"And while everyone knows about Santorini and Mykonos, there are "
++"gorgeous lesser-known islands in Greece, too.";
+
+IntroParagraph.innerHTML = introParaData;
+
+}
+
+
+
+
 
     /**
      * This functionhanding event for conatct form
